9c1f000370abdf3a464b1961c768730352a35c3e
Geralt <Geralt@iv66.net>
2020-08-28 18:10:36 +0800

    feat: 导入lookin

---
+++ HYNewNest/Categories/UIWindow+Motion.h
@@ -0,0 +1,16 @@
+//
+//  UIWindow+Motion.h
+//  uknow
+//
+//  Created by uknow on 2019/7/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWindow (Motion)
+
+@property (nonatomic, strong) UIView *backView;
+
+@property (nonatomic, strong) UIView *buttonBackView;
+
+@end
