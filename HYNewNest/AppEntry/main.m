636455185e2907a7854a86d2c1ebce87d80e2d2f
Geralt <Geralt@iv66.net>
2021-09-23 14:00:56 +0800

    移除听云

--- HYNewNest/AppEntry/main.m
+++ HYNewNest/AppEntry/main.m
@@ -8,18 +8,12 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
-#import "tingyunApp/NBSAppAgent.h"
 
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
-        
-#ifndef DEBUG
-        [NBSAppAgent setRedirectURL:@"app.tingyunfenxi.com"];
-        [NBSAppAgent startWithAppID:@"8c0c339fa2644e58881219d5b6a0d042"];
-#endif
     }
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
