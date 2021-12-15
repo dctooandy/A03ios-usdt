adfdc903bacc56b7e390c5bad343a5b453c46805
andy <andy@ct88.com.tw>
2021-12-15 17:42:15 +0800

    [修正]
    高可用域名修正

--- HYNewNest/Features(主代码)/BASE/Networking/AppdelegateManager.m
+++ HYNewNest/Features(主代码)/BASE/Networking/AppdelegateManager.m
@@ -47,7 +47,7 @@
                 return @[@"https://h5.918rr.com/_glaxy_1e3c3b_/"];
                 break;
             case IVNEnvironmentPublish:
-                return  @[@"https://wrd.58baili.com/pro/_glaxy_1e3c3b_/", @"https://m.jkfjdfe.com:9188/_glaxy_1e3c3b_/", @"https://m.zncjdhvs.com:9188/_glaxy_1e3c3b_/", @"https://m.pkyorjhn.com:9188/_glaxy_1e3c3b_/"];
+                return @[@"https://wrd.58baili.com/pro/_glaxy_1e3c3b_/", @"https://m.pkyorjhn.com:9188/_glaxy_1e3c3b_/"];
                 break;
             default:
                 return @[@"http://www.pt-gateway.com/_glaxy_1e3c3b_/"];
