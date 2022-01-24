b20b4a0f5e3ee9dd3e1cff147dc26a1e45343ac2
andy <andy@ct88.com.tw>
2022-01-24 14:43:34 +0800

    [修正]
    下雨时间精算到秒

--- HYNewNest/Utility(工具类)/BTTAnimationPopView/RedPacketsInfoModel.m
+++ HYNewNest/Utility(工具类)/BTTAnimationPopView/RedPacketsInfoModel.m
@@ -19,9 +19,16 @@
     NSDate *serverDate = [NSDate dateWithTimeIntervalSince1970:[self.serverTimestamp intValue]];
     NSString *stringFromDate = [dateFormatter stringFromDate:serverDate];
     NSString *firstStartHour = [self.firstStartAt substringToIndex:2];
+    NSString *firstStartMins = [self.firstStartAt substringWithRange:NSMakeRange(3, 2)];
+    NSString *firstStartSec = [self.firstStartAt substringWithRange:NSMakeRange(6, 2)];
     NSString *secondStartHour = [self.secondStartAt substringToIndex:2];
-    int isFirstToSecond = ([secondStartHour intValue] - [firstStartHour intValue]) * 3600;
-    int isSecondToFirst = (24 - [secondStartHour intValue] + [firstStartHour intValue]) * 3600;
+    NSString *secondStartMins = [self.secondStartAt substringWithRange:NSMakeRange(3, 2)];
+    NSString *secondStartSec = [self.secondStartAt substringWithRange:NSMakeRange(6, 2)];
+    int isFirstToSecond = ([secondStartHour intValue] * 3600 + [secondStartMins intValue] * 60 + [secondStartSec intValue] -
+                           ([firstStartHour intValue] * 3600 + [firstStartMins intValue] * 60 + [firstStartSec intValue]));
+    
+    int isSecondToFirst = 24 * 3600 - ([secondStartHour intValue] * 3600 + [secondStartMins intValue] * 60 + [secondStartSec intValue]) +
+    ([firstStartHour intValue] * 3600 + [firstStartMins intValue] * 60 + [firstStartSec intValue]);
     if ([self.status isEqualToString:@"1"])
     {
         if ([stringFromDate intValue] < [firstStartHour intValue])
