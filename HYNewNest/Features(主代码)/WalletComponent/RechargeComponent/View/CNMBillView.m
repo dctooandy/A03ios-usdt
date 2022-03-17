c5e8cad6296bd994cbb5e626b583eb52b6e5db9d
cean <>
2022-03-17 17:55:42 +0800

    急速订单UI

---
+++ HYNewNest/Features(主代码)/WalletComponent/RechargeComponent/View/CNMBillView.m
@@ -0,0 +1,20 @@
+//
+//  CNMBillView.m
+//  HYNewNest
+//
+//  Created by cean on 3/17/22.
+//  Copyright © 2022 BYGJ. All rights reserved.
+//
+
+#import "CNMBillView.h"
+
+@implementation CNMBillView
+
+- (void)loadViewFromXib {
+    [super loadViewFromXib];
+    self.statusBtn.layer.cornerRadius = 16;
+    self.statusBtn.layer.borderColor = self.statusBtn.titleLabel.textColor.CGColor;
+    self.statusBtn.layer.borderWidth = 1;
+    self.statusBtn.layer.masksToBounds = YES;
+}
+@end
